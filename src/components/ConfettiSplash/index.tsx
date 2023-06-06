b717a2c3d2bf27dcbea7fed0eb3833e97b43a8c2
ayratdeveloper <ayratdeveloper@gmail.com>
2023-06-06 14:06:45 +0400

    added react confetti

---
+++ src/components/ConfettiSplash/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import Confetti from 'react-confetti';
+
+
+const ConfettiSplash: FC<{ confetti: boolean }> = ({ confetti }) => (
+  <div className={`opacity-on ${confetti ? '' : 'opacity-off'}`}>
+    <Confetti />
+  </div>
+);
+
+
+export default ConfettiSplash;
